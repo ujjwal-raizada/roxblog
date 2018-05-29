a0c6d517866f2f4aec4c57ac6245cffa436f38e1
ujjwalrox <ujjwalraizada@gmail.com>
2018-05-30 02:39:41 +0530

    Added better UI, Added Avatars for users, D.O.B range issue sorted

--- blog/forms.py
+++ blog/forms.py
@@ -1,10 +1,12 @@
 from django import forms
+import datetime
 
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 from django.contrib.admin.widgets import AdminDateWidget
 
 from .models import blog, Author
+
 
 
 
@@ -18,7 +20,7 @@ class SignUpForm(UserCreationForm):
 	first_name = forms.CharField(max_length=30, required=False, help_text='Optional.')
 	last_name = forms.CharField(max_length=30, required=False, help_text='Optional.')
 	email = forms.EmailField(max_length=254, help_text='Required. Inform a valid email address.')
-	date_of_birth = forms.DateField(widget = forms.SelectDateWidget(),)
+	date_of_birth = forms.DateField(widget = forms.SelectDateWidget(years=range(1900, datetime.date.today().year)),)
 	class Meta:
 		model = User
 		fields = ('username', 'first_name', 'last_name', 'date_of_birth', 'email', 'password1', 'password2', )
